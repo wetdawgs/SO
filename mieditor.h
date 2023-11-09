b2970fa6115be9c7f441aa35217bdbb829a6369f
David Alvarado Torres <davidalvaradotorres@Davids-MacBook-Pro.local>
2023-11-08 21:36:50 -0600

    añadiendo header para los imports y las directivas

---
+++ mieditor.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+#define KWHT  "\x1B[37m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KBLU  "\x1B[34m"
